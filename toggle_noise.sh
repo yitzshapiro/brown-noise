35ae014beb3700e982af83fef6d02aca1613568f
Ulas Can Cengiz <721050+ulsc@users.noreply.github.com>
2023-04-27 23:46:41 +0200

    Add `toggle_noise.sh`

---
+++ toggle_noise.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Check if the brown_noise process is running
+pid=$(pgrep -f "./brown_noise")
+
+if [ -z "$pid" ]; then
+  # If the process is not running, start it
+  nohup ./brown_noise > /dev/null 2>&1 &
+  echo "Brown noise started."
+else
+  # If the process is running, kill it
+  kill $pid
+  echo "Brown noise stopped."
+fi
